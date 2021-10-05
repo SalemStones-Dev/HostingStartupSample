858f34d20bf328cd4c55b8bf82323a241539f884
Donnie Hale <donnie.hale@awh.net>
2021-10-05 16:57:53 -0400

    Updated to RC1 - required some using statements since implicit usings are now opt-in. Using the 'ASPNETCORE_HOSTINGSTARTUPASSEMBLIES' causes hosting startup assemblies to work.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,13 @@
 using Interface;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+Console.WriteLine($"Target Framework: {Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName!}");
+Console.WriteLine($"Startup assemblies: {Environment.GetEnvironmentVariable("ASPNETCORE_HOSTINGSTARTUPASSEMBLIES")}");
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -10,5 +18,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/", ([FromServices] IHandler handler) => handler.HandleRequest("Hello World!"));
+
+var assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
 app.Run();
--- Library/Handler.cs
+++ Library/Handler.cs
@@ -1,4 +1,5 @@
 ﻿using Interface;
+using System;
 
 namespace Library;
 
