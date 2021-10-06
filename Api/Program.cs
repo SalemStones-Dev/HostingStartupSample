308ce4c82bc84c8a6cde6c1e4476a586ccf107b7
Donnie Hale <donnie.hale@awh.net>
2021-10-06 08:41:40 -0400

    Set environment variable programmatically. Added comment on what works.

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,6 +6,17 @@ using System;
 using System.Reflection;
 using System.Runtime.Versioning;
 
+// See: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/platform-specific-configuration?view=aspnetcore-6.0
+// Also see Twitter thread: https://twitter.com/adhalejr/status/1444035731073212421?s=20
+//
+// Previous asp.net core versions and the docs linked above (as of 2021-10-05) indicate that other
+// mechanisms besides an environment variable should cause hosting startup assemblies to be loaded.
+// But as of .NET 6.0 / ASP.NET Core 6.0 RC1, that's not the case.
+//
+// This environment variable causes the call to "CreateBuilder" to load the specified
+// hosting startup assemblies and call their IHostingStartup implementations.
+Environment.SetEnvironmentVariable("ASPNETCORE_HOSTINGSTARTUPASSEMBLIES", "Library");
+
 Console.WriteLine($"Target Framework: {Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName!}");
 Console.WriteLine($"Startup assemblies: {Environment.GetEnvironmentVariable("ASPNETCORE_HOSTINGSTARTUPASSEMBLIES")}");
 
@@ -18,7 +29,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/", ([FromServices] IHandler handler) => handler.HandleRequest("Hello World!"));
-
-var assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
 app.Run();
